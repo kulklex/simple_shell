f8753e9dc5fb0ecd8489c83ecc45cd050d95d902
JUSTICE <111039610+BallerJet@users.noreply.github.com>
2022-11-17 23:41:43 +0100

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,148 +1,152 @@
 #include "shell.h"
+
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias(char *var_name, char *value);
+void print_alias(alias_t *alias);
+
 /**
- * hosh_setenv - set an environment variable
+ * shellby_alias - Builtin command that either prints all aliases, specific
+ * aliases, or sets an alias.
+ * @args: An array of arguments.
+ * @front: A double pointer to the beginning of args.
  *
- * @args: argument array
- *
- * Return: 0 on success, -1 on failure
+ * Return: If an error occurs - -1.
+ *         Otherwise - 0.
  */
-int hosh_setenv(char **args)
+int shellby_alias(char **args, char __attribute__((__unused__)) **front)
 {
-char *envp;
-int i, j, len;
-if (args[1] == NULL || args[2] == NULL || args[1][0] == '\0')
-{
-_puts("setenv syntax: setenv VARIABLE VALUE\n");
-return (-1);
+	alias_t *temp = aliases;
+	int i, ret = 0;
+	char *value;
+
+	if (!args[0])
+	{
+		while (temp)
+		{
+			print_alias(temp);
+			temp = temp->next;
+		}
+		return (ret);
+	}
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		value = _strchr(args[i], '=');
+		if (!value)
+		{
+			while (temp)
+			{
+				if (_strcmp(args[i], temp->name) == 0)
+				{
+					print_alias(temp);
+					break;
+				}
+				temp = temp->next;
+			}
+			if (!temp)
+				ret = create_error(args + i, 1);
+		}
+		else
+			set_alias(args[i], value);
+	}
+	return (ret);
 }
-_unsetenv(args[1]);
-len = 0;
-for (i = 2; args[i] != NULL; i++)
-for (j = 0; args[i][j] != '\0'; j++, len++)
-;
-envp = smart_alloc(sizeof(char) * (len + i - 1));
-envp = _strcpy(envp, args[1]);
-envp = str_concat(envp, "=");
-for (i = 2; args[i] != NULL; i++)
+
+/**
+ * set_alias - Will either set an existing alias 'name' with a new value,
+ * 'value' or creates a new alias with 'name' and 'value'.
+ * @var_name: Name of the alias.
+ * @value: Value of the alias. First character is a '='.
+ */
+void set_alias(char *var_name, char *value)
 {
-envp = str_concat(envp, args[i]);
-if (args[i + 1] != NULL)
-envp = str_concat(envp, " ");
+	alias_t *temp = aliases;
+	int len, j, k;
+	char *new_value;
+
+	*value = '\0';
+	value++;
+	len = _strlen(value) - _strspn(value, "'\"");
+	new_value = malloc(sizeof(char) * (len + 1));
+	if (!new_value)
+		return;
+	for (j = 0, k = 0; value[j]; j++)
+	{
+		if (value[j] != '\'' && value[j] != '"')
+			new_value[k++] = value[j];
+	}
+	new_value[k] = '\0';
+	while (temp)
+	{
+		if (_strcmp(var_name, temp->name) == 0)
+		{
+			free(temp->value);
+			temp->value = new_value;
+			break;
+		}
+		temp = temp->next;
+	}
+	if (!temp)
+		add_alias_end(&aliases, var_name, new_value);
 }
-if (_addenv(envp, args[1]) != 0)
-return (-1);
-return (0);
+
+/**
+ * print_alias - Prints the alias in the format name='value'.
+ * @alias: Pointer to an alias.
+ */
+void print_alias(alias_t *alias)
+{
+	char *alias_string;
+	int len = _strlen(alias->name) + _strlen(alias->value) + 4;
+
+	alias_string = malloc(sizeof(char) * (len + 1));
+	if (!alias_string)
+		return;
+	_strcpy(alias_string, alias->name);
+	_strcat(alias_string, "='");
+	_strcat(alias_string, alias->value);
+	_strcat(alias_string, "'\n");
+
+	write(STDOUT_FILENO, alias_string, len);
+	free(alias_string);
 }
 /**
- * hosh_unsetenv - unset an environment variable
+ * replace_aliases - Goes through the arguments and replace any matching alias
+ * with their value.
+ * @args: 2D pointer to the arguments.
  *
- * @args: argument array
- *
- * Return: 0 if success, -1 if failure
+ * Return: 2D pointer to the arguments.
  */
-int hosh_unsetenv(char **args)
+char **replace_aliases(char **args)
 {
-if (args[1] == NULL)
-return (-1);
-return (_unsetenv(args[1]));
-}
-/**
- * hosh_exit - exit the current child process & free what must be free'd
- * @args: Double pointer to the command and given args
- * @input: The original input from the user
- *
- * Return: 0 if success, 1 if failure
- */
-int hosh_exit(char **args, char *input)
-{
-int status;
-if (args[1] == NULL)
-{
-free(input);
-free(args);
-_exit(0);
-}
-else
-{
-status = _atoi(args[1]);
-if (status == -1)
-{
-_puts("Illegal number\n");
-return (1);
-}
-free(input);
-free(args);
-_exit(status);
-}
-return (1);
-}
-/**
- * hosh_printenv - print the list of environment variables
- * @args: Arguments passed to hosh_printenv
- *
- * Return: 0 on success, 1 on failure
- */
-int hosh_printenv(char **args)
-{
-char c;
-int i, j;
-if (args[1] == NULL)
-{
-for (i = 0; environ[i]; i++)
-{
-for (j = 0; environ[i][j]; j++)
-{
-c = environ[i][j];
-_putchar(c);
-}
-_putchar('\n');
-}
-return (0);
-}
-return (1);
-}
-/**
- * hosh_help - Print out help for given commands or list all builtins available
- * @args: List of arguments passed to hosh_help
- *
- * Return: 0 on success, 1 on failure
- */
-int hosh_help(char **args)
-{
-help_t builtins[] = {
-{"exit", help_exit},
-{"setenv", help_setenv},
-{"unsetenv", help_unsetenv},
-{"env", help_env},
-{"help", help_help},
-{NULL, NULL}
-};
-int i, j, arglen, foundhelp;
-foundhelp = 0;
-i = 1;
-if (args[1] == NULL)
-{
-print_help();
-return (0);
-}
-while (args[i] != NULL)
-{
-j = 0;
-while (builtins[j].name != NULL)
-{
-arglen = _strlen(args[i]);
-if (_strncmp(builtins[j].name, args[i], arglen) == 0)
-{
-foundhelp++;
-builtins[j].func();
-break;
-}
-j++;
-}
-i++;
-}
-if (foundhelp > 0)
-return (0);
-_puts("Sorry. Could not find help for that command.\n");
-return (1);
+	alias_t *temp;
+	int i;
+	char *new_value;
+
+	if (_strcmp(args[0], "alias") == 0)
+		return (args);
+	for (i = 0; args[i]; i++)
+	{
+		temp = aliases;
+		while (temp)
+		{
+			if (_strcmp(args[i], temp->name) == 0)
+			{
+				new_value = malloc(sizeof(char) * (_strlen(temp->value) + 1));
+				if (!new_value)
+				{
+					free_args(args, args);
+					return (NULL);
+				}
+				_strcpy(new_value, temp->value);
+				free(args[i]);
+				args[i] = new_value;
+				i--;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+
+	return (args);
 }
